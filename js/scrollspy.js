b6854b6b9b7284cb1c0b15d58e2fdaa9f68f7e9c
user.MuhammadShahrukh <shahrukh.liaquat16@gmail.com>
2020-04-29 21:37:35 +0500

    iniitial

---
+++ js/scrollspy.js
@@ -0,0 +1,11 @@
+$('body').scrollspy({ 
+    target: 'navbarresponsive',
+    offset: 80
+});
+
+// Go to specific div with animation
+$('.scrollto').click(function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({ scrollTop: $($anchor.attr('href')).offset().top - 40  }, 1500);
+    event.preventDefault();
+});
